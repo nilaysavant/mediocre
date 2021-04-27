7ac2dacfc9326c7383bf24eba2b5a60b6cd924ac
Nilay Savant <nilayofficial@gmail.com>
2021-04-28 01:32:22 +0530

    WIP create a dark mode design

--- src/App.tsx
+++ src/App.tsx
@@ -7,57 +7,42 @@ function App() {
   const [receivedText, setReceivedText] = useState<string>('')
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="./res/logo512.png"
-          className="App-logo"
-          alt="logo-res-512png"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="dark"
+      style={{ width: '100vw', height: '100vh', background: '#1f1f1f', color: '#ededed' }}
+    >
+      <div className="flex flex-col items-center p-4">
+        <button
+          className="bg-gray-700 px-6 py-1 rounded-md"
+          onClick={async () => {
+            const res: { markup: string } = await tauri.invoke(
+              'parse_md_to_mu',
+              {
+                mdString: sendText,
+              }
+            )
+            console.log('🚀 ~ file: App.tsx ~ line 32 ~ onClick={ ~ res', res)
+            setReceivedText(res.markup)
+          }}
         >
-          Learn React
-        </a>
-        <div className="flex flex-col mt-4 items-center">
-          <button
-            className="bg-gray-700 px-6 py-1 rounded-md"
-            onClick={async () => {
-              const res: { markup: string } = await tauri.invoke(
-                'parse_md_to_mu',
-                {
-                  mdString: sendText,
-                }
-              )
-              console.log('🚀 ~ file: App.tsx ~ line 32 ~ onClick={ ~ res', res)
-              setReceivedText(res.markup)
+          Send
+        </button>
+        <div className="flex justify-items-center">
+          <textarea
+            className="m-4 bg-gray-900 resize-none w-72 p-2 h-28 border border-gray-600 rounded-md"
+            placeholder="Enter text to send"
+            onChange={(e) => setSendText(e.target.value)}
+          />
+          <div
+            className="m-4 bg-gray-900 w-72 p-2 h-28 border border-gray-600 rounded-md text-left"
+            style={{
+              whiteSpace: 'pre',
             }}
           >
-            Send
-          </button>
-          <div className="flex justify-items-center">
-            <textarea
-              className="m-4 bg-gray-900 resize-none w-72 p-2 h-28 border border-gray-600 rounded-md"
-              placeholder="Enter text to send"
-              onChange={(e) => setSendText(e.target.value)}
-            />
-            <div
-              className="m-4 bg-gray-900 w-72 p-2 h-28 border border-gray-600 rounded-md text-left"
-              style={{
-                whiteSpace: 'pre',
-              }}
-            >
-              {receivedText || 'N/A'}
-            </div>
+            {receivedText || 'N/A'}
           </div>
         </div>
-      </header>
+      </div>
     </div>
   )
 }
