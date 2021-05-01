b10ce7675f4ec69dc0492ba8143f8f985dc5a42f
Nilay Savant <nilayofficial@gmail.com>
2021-05-02 02:06:42 +0530

    Install react icons, edits to file menu items

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import { Button, IconButton } from '@chakra-ui/button'
 import { useColorMode } from '@chakra-ui/color-mode'
-import { ChevronDownIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { ChevronDownIcon, Icon, MoonIcon, SunIcon } from '@chakra-ui/icons'
 import { Box } from '@chakra-ui/layout'
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu'
-import React from 'react'
+import { IoIosSave } from 'react-icons/io'
 
 function Topbar() {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -19,17 +20,38 @@ function Topbar() {
             borderRadius="sm"
             _focus={{
               outline: 'none',
-              borderBottom: '4px solid #616161'
+              borderBottom: '4px solid #616161',
             }}
           >
             File
           </MenuButton>
-          <MenuList borderRadius="sm">
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
+          <MenuList borderRadius="sm" fontSize="md">
+            <MenuItem display="flex" justifyContent="space-between">
+              <Box>New</Box>
+              <Box
+                color="gray.700"
+                fontSize="sm"
+                background="gray.300"
+                rounded="sm"
+                paddingX="2"
+              >
+                Ctrl+N
+              </Box>
+            </MenuItem>
+            <MenuItem display="flex" justifyContent="space-between">
+              <Box>Save</Box>
+              <Box
+                color="gray.700"
+                fontSize="sm"
+                background="gray.300"
+                rounded="sm"
+                paddingX="2"
+              >
+                Ctrl+S
+              </Box>
+            </MenuItem>
+            <MenuItem>Duplicate</MenuItem>
+            <MenuItem>Quit</MenuItem>
           </MenuList>
         </Menu>
       </div>
@@ -38,7 +60,7 @@ function Topbar() {
         borderRadius="sm"
         _focus={{
           outline: 'none',
-          borderBottom: '4px solid #616161'
+          borderBottom: '4px solid #616161',
         }}
         aria-label="Toggle theme"
         onClick={toggleColorMode}
