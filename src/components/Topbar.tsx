b7806067a4daf1fa21acabf0e25b52fb5b1d113a
Nilay Savant <nilayofficial@gmail.com>
2021-05-02 00:36:43 +0530

    Minor edits and refactor

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,19 +4,13 @@ import React from 'react'
 
 function Topbar() {
   const { colorMode, toggleColorMode } = useColorMode()
-  console.log('🚀 ~ file: Topbar.tsx ~ line 7 ~ Topbar ~ colorMode', colorMode)
 
   return (
     <div className="flex w-full">
       <button className="bg-gray-700 px-6 py-1 rounded-sm m-1">File</button>
       <button className="bg-gray-700 px-6 py-1 rounded-sm m-1">Edit</button>
       <button className="bg-gray-700 px-6 py-1 rounded-sm m-1">Help</button>
-      <Button
-        onClick={() => {
-          console.log('kk')
-          toggleColorMode()
-        }}
-      >
+      <Button className="dark:text-white text-black" onClick={toggleColorMode}>
         Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
       </Button>
     </div>
