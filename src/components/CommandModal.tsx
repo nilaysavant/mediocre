6476f86e0c0024b99606ac32b7722529fcd70bf8
Nilay Savant <nilayofficial@gmail.com>
2021-05-25 00:32:01 +0530

    edits to colos and styling

--- src/components/CommandModal.tsx
+++ src/components/CommandModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, List, ListIcon, ListItem, UnorderedList } from '@chakra-ui/layout'
 import {
   Modal,
@@ -22,14 +22,25 @@ import { Button } from '@chakra-ui/button'
 import { AiOutlineEnter } from 'react-icons/ai'
 import { IconType } from 'react-icons/lib'
 
+const commandItems = [0, 1, 2, 3, 4]
+
 export type CommandItemProps = {
   id: string
   title: string
   subtitle: string
   icon: IconType
+  focused?: boolean
+  selected?: boolean
 }
 
-function CommandItem({ id, title, subtitle, icon }: CommandItemProps) {
+function CommandItem({
+  id,
+  title,
+  subtitle,
+  icon,
+  focused = false,
+  selected = false,
+}: CommandItemProps) {
   return (
     <ListItem
       id={id}
@@ -43,7 +54,7 @@ function CommandItem({ id, title, subtitle, icon }: CommandItemProps) {
       borderRadius="md"
       userSelect="none"
       cursor="pointer"
-      background="gray.800"
+      background={focused ? '#0072a3' : selected ? '#1A202C' : '#242933'}
     >
       <ListIcon as={AddIcon} color="green.500" fontSize="lg" />
       <Box flex="1" paddingX="0.5">
@@ -65,6 +76,32 @@ export type CommandModalProps = {
 }
 
 function CommandModal({ isOpen, onClose }: CommandModalProps) {
+  const [focusedItem, setFocusedItem] = useState(0)
+  const [selectedItem, setSelectedItem] = useState(0)
+
+  const handleInputKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (event.key) {
+      case 'ArrowUp': {
+        setFocusedItem((old) =>
+          old - 1 < 0 ? commandItems.length - 1 : old - 1
+        )
+        break
+      }
+      case 'ArrowDown': {
+        setFocusedItem((old) =>
+          old + 1 > commandItems.length - 1 ? 0 : old + 1
+        )
+        break
+      }
+      case 'Enter': {
+        setSelectedItem(focusedItem)
+        break
+      }
+      default:
+        break
+    }
+  }
+
   return (
     <Modal
       closeOnOverlayClick={true}
@@ -87,18 +124,21 @@ function CommandModal({ isOpen, onClose }: CommandModalProps) {
               _focus={{ border: 'none' }}
               width="full"
               // autoFocus={true}
+              onKeyDown={handleInputKeydown}
             />
           </InputGroup>
         </ModalHeader>
-        <ModalBody pb={6} paddingX="4">
+        <ModalBody pb={4} paddingX="4">
           <List spacing={3}>
-            {[0, 1, 2, 3, 4].map((v, i) => (
+            {commandItems.map((v, i) => (
               <CommandItem
                 key={`a${i}`}
                 id="add"
                 title="Note Manager"
                 subtitle="Add new Mediocre Note. Make something awesome!"
                 icon={AiOutlineEnter}
+                focused={focusedItem === i}
+                selected={selectedItem === i}
               />
             ))}
           </List>
