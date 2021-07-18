2e33ef54460466436272bf180118aa2abc5c7c38
Nilay Savant <nilayofficial@gmail.com>
2021-07-19 00:11:41 +0530

    Item menu styling edits

--- src/components/SideBar/ItemMenu.tsx
+++ src/components/SideBar/ItemMenu.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   ButtonGroup,
   List,
+  ListIcon,
   ListItem,
   Menu,
   MenuButton,
@@ -21,7 +22,31 @@ import {
   Text,
   useDisclosure,
 } from '@chakra-ui/react'
-import { DeleteIcon } from '@chakra-ui/icons'
+import { CopyIcon, DeleteIcon } from '@chakra-ui/icons'
+import { IconType } from 'react-icons/lib'
+
+const menuItems: {
+  id: string
+  icon: any
+  label: string
+  command?: string
+  onClick?: () => void
+}[] = [
+  {
+    id: 'duplicate',
+    icon: CopyIcon,
+    label: 'Duplicate',
+    command: '',
+    onClick: () => console.log('open clicked'),
+  },
+  {
+    id: 'delete',
+    icon: DeleteIcon,
+    label: 'Delete',
+    command: 'Del',
+    onClick: () => console.log('del clicked'),
+  },
+]
 
 export type ItemMenuProps = {
   /** uses render props ref: https://reactjs.org/docs/render-props.html */
@@ -48,18 +73,49 @@ const ItemMenu = ({ children, popoverProps }: ItemMenuProps) => {
     >
       <PopoverTrigger>{children({ isOpen, onOpen, onClose })}</PopoverTrigger>
       <PopoverContent
-        w="36"
+        w="full"
         borderRadius="none"
         _focus={{
           boxShadow: 'none',
         }}
       >
-        <PopoverBody>
-          <List borderRadius="none" fontSize="xs" minWidth="36">
-            <ListItem>
-              <DeleteIcon />
-              Delete
-            </ListItem>
+        <PopoverBody p="0" minWidth="36" maxWidth="72">
+          <List
+            borderRadius="none"
+            fontSize="xs"
+            flex="1"
+            minHeight="0"
+            spacing="0.5"
+            color="#e3e3e3"
+            w="full"
+          >
+            {menuItems.map((item) => (
+              <ListItem
+                key={item.id}
+                display="flex"
+                alignItems="center"
+                py="0.5"
+                px="2"
+                userSelect="none"
+                cursor="pointer"
+                w="full"
+                _hover={{
+                  bg: '#fafafa0d',
+                }}
+                _active={{
+                  bg: '#fafafa1f',
+                }}
+                onClick={item.onClick}
+              >
+                <ListIcon
+                  as={item.icon}
+                  color="icon.dark"
+                  fontSize="small"
+                  marginRight="1.5"
+                />
+                <Text isTruncated>{item.label}</Text>
+              </ListItem>
+            ))}
           </List>
         </PopoverBody>
       </PopoverContent>
