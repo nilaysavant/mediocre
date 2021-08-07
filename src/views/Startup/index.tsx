d9da167173e5e314410f54380590e4d2fcc0bd8b
Nilay Savant <nilayofficial@gmail.com>
2021-08-07 23:16:23 +0530

    Update startup view, minor fixes to body style

--- src/views/Startup/index.tsx
+++ src/views/Startup/index.tsx
@@ -1,9 +1,47 @@
-import { Box } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
+import { Box, Kbd, Text } from '@chakra-ui/react'
+import AppLogoIcon from 'src/icons/AppLogoIcon'
 
 const Startup = () => {
   return (
-    <Box flex="1" minWidth="0" display="flex" rounded="none" minHeight="0">
-      Startup
+    <Box
+      flex="1"
+      minWidth="0"
+      rounded="none"
+      minHeight="0"
+      bg="bg.dark.600"
+      position="relative"
+    >
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -80%)"
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+      >
+        <Box display="flex" flexDir="column" alignItems="center">
+          <AppLogoIcon fontSize="10rem" />
+          <Text>Mediocre Editor</Text>
+        </Box>
+        <Box>
+          <Text
+            color="gray.500"
+            fontSize="md"
+            display="flex"
+            alignItems="center"
+            whiteSpace="nowrap"
+            isTruncated
+          >
+            Click on{' '}
+            <Kbd py="0.5" mx="1.5">
+              <AddIcon fontSize="xs" />
+            </Kbd>{' '}
+            to create a new document
+          </Text>
+        </Box>
+      </Box>
     </Box>
   )
 }
