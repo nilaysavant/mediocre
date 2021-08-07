f3d3c6d8d9d64cffaf41b1f2f54e1905134d6f21
Nilay Savant <nilayofficial@gmail.com>
2021-08-07 23:36:13 +0530

    Update settings view WIP

--- src/views/Settings/index.tsx
+++ src/views/Settings/index.tsx
@@ -1,9 +1,21 @@
-import { Box } from '@chakra-ui/react'
+import { SettingsIcon } from '@chakra-ui/icons'
+import { Box, Text } from '@chakra-ui/react'
 
 const Settings = () => {
   return (
-    <Box flex="1" minWidth="0" display="flex" rounded="none" minHeight="0">
-      Settings
+    <Box
+      flex="1"
+      minWidth="0"
+      rounded="none"
+      minHeight="0"
+      bg="bg.dark.600"
+      p="3"
+    >
+      <Box display="flex" alignItems="center" mb="1" fontSize="md">
+        <SettingsIcon mr="2" color="icon.dark.400" />
+        <Text>Settings</Text>
+      </Box>
+      <hr />
     </Box>
   )
 }
