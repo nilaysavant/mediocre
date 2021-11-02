5b58debcf25c4f3c636b5a0ef1e809d9caf69c26
Nilay Savant <nilayofficial@gmail.com>
2021-11-02 17:40:38 +0530

    Minor fixes

--- src-tauri/src/models/app_state.rs
+++ src-tauri/src/models/app_state.rs
@@ -19,20 +19,21 @@ pub struct AppDbState {
 
 impl AppDbState {
   /// Create a db instance
-  fn create_db(self, db_path: &PathBuf) -> PickleDb {
+  fn create_db(&self, db_path: &PathBuf) {
     PickleDb::new(
       db_path,
       PickleDbDumpPolicy::AutoDump,
       SerializationMethod::Bin,
-    )
+    );
   }
   /// Init a db instance (ie. create + load)
-  pub fn init(self, db_path: &PathBuf) -> Result<PickleDb, pickledb::error::Error> {
+  pub fn init(&mut self, db_path: &PathBuf) -> Result<(), pickledb::error::Error> {
     self.create_db(db_path);
-    PickleDb::load(
+    self.db = PickleDb::load(
       db_path,
       PickleDbDumpPolicy::AutoDump,
       SerializationMethod::Bin,
-    )
+    )?;
+    Ok(())
   }
 }
