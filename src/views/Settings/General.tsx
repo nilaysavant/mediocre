728e302547c3813b390e0bf80d6754e99601fcca
Nilay Savant <nilayofficial@gmail.com>
2021-11-03 19:20:22 +0530

    WIP create accordion sections in general settings

--- src/views/Settings/General.tsx
+++ src/views/Settings/General.tsx
@@ -1,11 +1,90 @@
-import { Box } from '@chakra-ui/react'
+import {
+  Accordion,
+  AccordionButton,
+  AccordionButtonProps,
+  AccordionIcon,
+  AccordionItem,
+  AccordionItemProps,
+  AccordionPanel,
+  AccordionPanelProps,
+  Box,
+} from '@chakra-ui/react'
+import { getUniqueIdV4 } from 'src/utils/idGenerator'
+
+export type SectionAccordionItemProps = {
+  sectionTitle: React.ReactNode
+  sectionContent: React.ReactNode
+  accordionItemProps?: AccordionItemProps
+  accordionButtonProps?: AccordionButtonProps
+  accordionPanelProps?: AccordionPanelProps
+}
+
+/**
+ * ### Section `accordion` item component.
+ *
+ * For sections in settings etc.
+ *
+ * - `sectionTitle`: Title of the section
+ * - `sectionContent`: Content of the section
+ */
+const SectionAccordionItem = ({
+  sectionTitle,
+  sectionContent,
+  accordionItemProps,
+  accordionButtonProps,
+  accordionPanelProps,
+}: SectionAccordionItemProps) => {
+  return (
+    <AccordionItem {...accordionItemProps}>
+      <AccordionButton {...accordionButtonProps}>
+        <Box flex="1" textAlign="left">
+          {sectionTitle}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+      <AccordionPanel fontSize="sm" {...accordionPanelProps}>
+        {sectionContent}
+      </AccordionPanel>
+    </AccordionItem>
+  )
+}
+
+const generalSettingsSections: {
+  id: string
+  title: React.ReactNode
+  content: React.ReactNode
+}[] = [
+  {
+    id: getUniqueIdV4(),
+    title: 'Cloud Sync',
+    content: <Box>Cloud Sync Content</Box>,
+  },
+  {
+    id: getUniqueIdV4(),
+    title: 'Privacy',
+    content: <Box>Privacy Content</Box>,
+  },
+]
 
 /**
  * General Settings
  * @returns
  */
 const General = () => {
-  return <Box>General Settings</Box>
+  return (
+    <Accordion defaultIndex={[0]} allowMultiple>
+      {generalSettingsSections.map((item, idx) => (
+        <SectionAccordionItem
+          key={item.id}
+          sectionTitle={item.title}
+          sectionContent={item.content}
+          accordionItemProps={{
+            borderTop: idx === 0 ? 'none' : undefined,
+          }}
+        />
+      ))}
+    </Accordion>
+  )
 }
 
 export default General
