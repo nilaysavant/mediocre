6221c668c6136fc8d8ee45867768240dd16f74af
Nilay Savant <nilayofficial@gmail.com>
2021-11-03 20:56:50 +0530

    Minor rename

--- src/views/Settings/General/CloudSync/index.tsx
+++ src/views/Settings/General/CloudSync/index.tsx
@@ -1,61 +1,7 @@
-import { Button } from '@chakra-ui/button'
-import {
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-} from '@chakra-ui/form-control'
-import { Input } from '@chakra-ui/input'
-import { Formik, Form } from 'formik'
+import GitSyncForm from './GitSyncForm'
 
 const CloudSync = () => {
-  return (
-    <Formik
-      initialValues={{ name: 'Sasuke' }}
-      validate={(values) => {
-        if (!values.name)
-          return {
-            name: 'Required',
-          }
-      }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
-          actions.setSubmitting(false)
-        }, 1000)
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <Form>
-          <FormControl isInvalid={!!errors.name && touched.name}>
-            <FormLabel htmlFor="name">First name</FormLabel>
-            <Input
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="name"
-              placeholder="name"
-            />
-            <FormErrorMessage>{errors.name}</FormErrorMessage>
-          </FormControl>
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  )
+  return <GitSyncForm />
 }
 
 export default CloudSync
