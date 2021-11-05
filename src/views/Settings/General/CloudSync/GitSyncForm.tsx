9fe233e44efc42c838ff6fa4c61cc6f889076a09
Nilay Savant <nilayofficial@gmail.com>
2021-11-05 19:06:01 +0530

    WIP creating comp for stepper

--- src/views/Settings/General/CloudSync/GitSyncForm.tsx
+++ src/views/Settings/General/CloudSync/GitSyncForm.tsx
@@ -1,14 +1,18 @@
-import { Button } from '@chakra-ui/button'
 import {
   FormControl,
   FormErrorMessage,
   FormLabel,
 } from '@chakra-ui/form-control'
 import { Input } from '@chakra-ui/input'
+import { Circle, Flex, Stack } from '@chakra-ui/layout'
 import { Formik, Form } from 'formik'
 import SettingsButton from '../../SettingsButton'
 
-const GitSyncForm = () => {
+export type GitSyncFormProps = {
+  children?: React.ReactNode
+}
+
+const GitSyncForm = ({ children }: GitSyncFormProps) => {
   return (
     <Formik
       initialValues={{ sshKeyLocation: 'Sasuke' }}
@@ -47,9 +51,21 @@ const GitSyncForm = () => {
             />
             <FormErrorMessage>{errors.sshKeyLocation}</FormErrorMessage>
           </FormControl>
-          <SettingsButton mt={4} isLoading={isSubmitting} type="submit">
-            Submit
-          </SettingsButton>
+          <Flex alignItems="center" justifyContent="space-between" mt={4}>
+            <Stack direction="row" p="2">
+              <Circle bg="icon.dark.400" size="10px" />
+              <Circle bg="gray.600" size="10px" />
+              <Circle bg="gray.600" size="10px" />
+            </Stack>
+            <Stack direction="row">
+              <SettingsButton colorScheme="gray">
+                Back
+              </SettingsButton>
+              <SettingsButton isLoading={isSubmitting} type="submit">
+                Next
+              </SettingsButton>
+            </Stack>
+          </Flex>
         </Form>
       )}
     </Formik>
