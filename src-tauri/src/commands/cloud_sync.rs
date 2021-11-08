98b547d292dee911409a6253bcd5095a8e3b990e
Nilay Savant <nilayofficial@gmail.com>
2021-11-08 23:10:17 +0530

    Minor fixes

--- src-tauri/src/commands/cloud_sync.rs
+++ src-tauri/src/commands/cloud_sync.rs
@@ -1,7 +1,7 @@
 use log::info;
 use serde::{Deserialize, Serialize};
 
-use crate::models::{app_db_state::AppDbState, app_state::AppState, cloud_sync::CloudSync};
+use crate::{commands::fs, models::{app_db_state::AppDbState, app_state::AppState, cloud_sync::CloudSync}};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -17,6 +17,7 @@ pub fn test_git_clone_ssh(
   db_state: tauri::State<'_, AppDbState>,
 ) -> Result<TestGitCloneSshResponse, String> {
   info!("Testing git clone via ssh...");
+  std::fs::remove_dir_all("/tmp/git2-rs").ok();
   CloudSync::test_git_clone_ssh().map_err(|e| e.to_string())?;
   info!("Done!");
   Ok(TestGitCloneSshResponse {
